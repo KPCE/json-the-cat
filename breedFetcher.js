f036a67b0002809419c8dce5e398eccb5458d2bf
Jaime Reuss <jamesreuss1@gmail.com>
2021-01-14 22:48:02 +0000

    adding package, index for calling functin, refactored breedfetcher to move calling and input handling to index

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,22 @@
 const request = require('request');
 
-const breed = process.argv[2];
+//const breedname = process.argv[2];
 
-const breedFetcher = function(breed) {
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-    if (error) console.log("error: ", error);
-    const data = JSON.parse(body);
-    
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null); //passes the error if there is one, null for description
+    };
+    const data = JSON.parse(body); 
     if (data[0] === undefined) {
-      return console.log("error found, please enter a valid cat breed for search");
-    } else {
-      console.log(data[0].description);
-    }
-  
-  });
-};
+      callback("breed not found", null);
+      } else {
+      callback(null, data[0].description);
+      } 
+    })
+  };
 
-breedFetcher(breed);
+
+//breedFetcher(breed);
+
+module.exports = { fetchBreedDescription };
