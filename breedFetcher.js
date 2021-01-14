4c5d6e4df2d347ed429262f95989bcbdb8401c01
Jaime Reuss <jamesreuss1@gmail.com>
2021-01-14 22:49:34 +0000

    linted breedFetcher, index was already good

--- breedFetcher.js
+++ breedFetcher.js
@@ -6,15 +6,15 @@ const fetchBreedDescription = function(breedName, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
     if (error) {
       callback(error, null); //passes the error if there is one, null for description
-    };
-    const data = JSON.parse(body); 
+    }
+    const data = JSON.parse(body);
     if (data[0] === undefined) {
       callback("breed not found", null);
-      } else {
+    } else {
       callback(null, data[0].description);
-      } 
-    })
-  };
+    }
+  });
+};
 
 
 //breedFetcher(breed);
